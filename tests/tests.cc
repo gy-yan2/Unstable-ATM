aa433d25776feb0f65d9235d6f6f370e3f8b880e
gyuany <162180241+gyuany@users.noreply.github.com>
2026-02-27 16:06:26 -0600

    test

--- tests/tests.cc
+++ tests/tests.cc
@@ -85,6 +85,7 @@ TEST_CASE("Example: Print Prompt Ledger", "[ex-3]") {
   atm.PrintLedger("./prompt.txt", 12345678, 1234);
   REQUIRE(CompareFiles("./ex-1.txt", "./prompt.txt"));
 
+  // check account doesn't exist
   REQUIRE_THROWS_AS(
             atm.PrintLedger("./prompt.txt", 11112222, 1111), 
             std::invalid_argument
